9432ad50df30104e7a35aa79325f4c1650b54b42
HuynhLamPhuSi <sihpl@vng.com.vn>
2019-07-22 09:42:05 +0700

    Initial Commit

---
+++ ContactFriendMaking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ContactFriendMaking
+//
+//  Created by CPU11899 on 7/22/19.
+//  Copyright © 2019 CPU11899. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ContactFriendMakingTests/ContactFriendMakingTests.m
@@ -0,0 +1,37 @@
+//
+//  ContactFriendMakingTests.m
+//  ContactFriendMakingTests
+//
+//  Created by CPU11899 on 7/22/19.
+//  Copyright © 2019 CPU11899. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ContactFriendMakingTests : XCTestCase
+
+@end
+
+@implementation ContactFriendMakingTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
